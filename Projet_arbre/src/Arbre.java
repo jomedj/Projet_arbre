e4ce523a562648ce956d3ef959a62468fe960aef
Johann Medjo <johann.medjo@etu.univ-nantes.fr>
2018-10-10 11:53:38 +0200

    Ajout de méthode et suppression de feuille

--- Projet_arbre/src/Arbre.java
+++ Projet_arbre/src/Arbre.java
@@ -1,4 +1,20 @@
 
 public class Arbre {
-
+	private ArrayList<Noeud> noeuds;
+	
+	public Arbre() {
+		this.noeuds= new ArrayList<Noeud>();
+	}
+	
+	public boolean contains(int v) {
+		for(Noeud n : noeuds) {
+			if(n.getValue) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public ArrayList<Noeud> getNoeuds(){
+		return Noeuds;
+	}
 }
--- Projet_arbre/src/Noeud.java
+++ Projet_arbre/src/Noeud.java
@@ -1,4 +1,11 @@
 
 public class Noeud {
-
+	private Noeud fg;
+	private Noeud fd;
+	private int value;
+	
+	public Noeud(Noeud fg, Noeud fd) {
+		this.fg = fg;
+		this.fd = fd;
+	}
 }
